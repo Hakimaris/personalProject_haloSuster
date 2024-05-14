bf2fa51a86a7352b3950e37414e9366e218b7bf8
Hakim <Hakimfatih30@gmail.com>
2024-05-14 17:01:48 +0700

    stuff

--- handlers/itHandler.go
+++ handlers/itHandler.go
@@ -3,5 +3,7 @@ package handlers
 import "github.com/gofiber/fiber/v2"
 
 func GetITHandler(c *fiber.Ctx) error {
-    return c.SendString("This is the IT Handler")
+	return c.Status(200).JSON(fiber.Map{
+		"message": "im it handler!",
+	})
 }
