8eb54aa92607dd9dee7e49e46a2606cf30983fb4
Hakim <Hakimfatih30@gmail.com>
2024-05-14 18:02:01 +0700

    test smth

--- handlers/itHandler.go
+++ handlers/itHandler.go
@@ -1,8 +1,32 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	// "fmt"
+	// "strings"
+	// "time"
+	"HaloSuster/models"
+	"HaloSuster/db"
+
+	"github.com/gofiber/fiber/v2"
+	// "github.com/gofiber/fiber/v2/log"
+)
 
 func GetITHandler(c *fiber.Ctx) error {
+	return c.Status(200).JSON(fiber.Map{
+		"message": "im it handler!",
+	})
+}
+
+func ItRegister(c *fiber.Ctx) error {
+	it := new(models.ItModel)
+	if err := c.BodyParser(it); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"message": "error parsing body",
+		})
+	}
+	var registerResult models.ItModel
+	conn :=db.CreateConn()
+
 	return c.Status(200).JSON(fiber.Map{
 		"message": "im it handler!",
 	})
