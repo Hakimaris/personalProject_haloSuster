03b88c8173d42da85537b2288c3564fee3018f44
Hakim <Hakimfatih30@gmail.com>
2024-05-15 16:58:00 +0700

    feat:add another migration

---
+++ db/migrations/20240515095218_add_table_user.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS "user" (
+    "id" uuid UNIQUE NOT NULL DEFAULT (gen_random_uuid()) PRIMARY KEY,
+    "nip" bigint UNIQUE NOT NULL,
+    "name" varchar(50) NOT NULL,
+    "password" varchar(255),
+    "identityCardScanning" varchar(255),
+    "createdAt" timestamp NOT NULL DEFAULT(NOW()),
+    "updatedAt" timestamp NOT NULL DEFAULT(NOW())
+);
